ec5858605b2bc285b195e47a87e4de5f0995f8be
Guide Fari <guideg6@gmail.com>
2024-03-24 06:30:17 +0200

    feat: decode contentLength value, plus lil tes

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,8 +1,11 @@
 package rpc
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"strconv"
 )
 
 func EncodeMessage(msg any) string {
@@ -13,4 +16,24 @@ func EncodeMessage(msg any) string {
 	}
 
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) (int, error) {
+	// get content header
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return 0, errors.New("Did not find separator")
+	}
+
+	// header looks something like this
+	// Content-Length:  <number>
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return 0, err
+	}
+
+	_ = content
+
+	return contentLength, nil
 }
